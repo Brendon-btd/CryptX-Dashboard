3eafaf24de456634ad087848c09bd779f9708d56
Brendon.btd <brendontafadzwa19@gmail.com>
2025-11-04 21:04:39 +0200

    removed user profile on sidebar

--- cryptx/src/components/Layout/Sidebar.jsx
+++ cryptx/src/components/Layout/Sidebar.jsx
@@ -27,21 +27,7 @@ function Sidebar() {
       {/* The Navigation Bar Menus */}
       <nav className="flex-1 p-4 space-y-2 overflow-y-auto"></nav>
 
-      {/* User Profile */}
-      <div className="p-4 border-slate-50 dark:border-slate-700/50">
-        <div className="flex items-center space-x-3 p-3 rounded-xl bg-slate-500 dark:bg-slate-50">
-          <img src="" alt="" className="w-10 h-10 rounded-full ring-2 ring-slate-800" />
-
-          <div className="flex-1 min-w-0">
-            <div className="flex-1 min-w-0">
-                <p className="text-sm font-medium text-slate-800 dark:text-black truncate">
-                  Laurice 
-                  </p>
-                  <p className="text-xs text-slate-500 dark:text-slate-400 truncate">@laurice22</p>
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </div>
   );
 }
