21f4d1db33a2b4b65159c5a3a6edf7092c5de2a1
Brendon.btd <brendontafadzwa19@gmail.com>
2025-11-06 21:57:52 +0200

    made it responsive

--- cryptx/src/components/Layout/Sidebar.jsx
+++ cryptx/src/components/Layout/Sidebar.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { ChartPie, LayoutDashboard, LogOut, Mail, Settings, Wallet, WalletCards} from "lucide-react";
+import {
+  ChartPie,
+  LayoutDashboard,
+  LogOut,
+  Mail,
+  Settings,
+  Wallet,
+  WalletCards,
+} from "lucide-react";
 import Logo from "../../assets/Logo.png";
 
 const menuItems = [
@@ -50,27 +58,24 @@ const menuItems = [
 
 function Sidebar() {
   return (
-    <div
-      className="w-62 transition duration-300 ease-in-out bg-gray-500 dark:bg-slate-50 backdrop-blur-xl  border-slate-200/50
-     flex flex-col relative z-10"
-    >
-      <div className="p-6">
-        <div className="flex items-center">
+    <div className="w-20 sm:w-72 transition duration-300 ease-in-out bg-gray-500 dark:bg-slate-50 backdrop-blur-xl border-slate-200/50 flex flex-col relative z-10">
+      <div className="p-4 sm:p-6">
+        <div className="flex items-center justify-center sm:justify-start">
           <img
             src={Logo}
             alt="CryptX logo"
-            className="h-32 w-auto object-contain"
+            className="h-12 sm:h-32 w-auto object-contain"
           />
         </div>
       </div>
 
       {/* The Navigation Bar Menus */}
-      <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
+      <nav className="flex-1 p-2 sm:p-4 space-y-2 overflow-y-auto">
         {menuItems.map((item) => {
           return (
             <div key={item.id} className="relative">
               <button
-                className={`w-full flex items-center justify-between p-3 rounded-xl transition-all duration-700 cursor-pointer ${
+                className={`w-full flex items-center justify-center sm:justify-between p-3 rounded-xl transition-all duration-700 cursor-pointer ${
                   item.active
                     ? "text-[#6154F0] font-semibold"
                     : "text-gray-400 hover:text-[#6154F0]"
@@ -80,13 +85,13 @@ function Sidebar() {
                   <item.icon
                     className={`w-5 h-5 ${item.active ? "stroke-[2.5]" : ""}`}
                   />
-                  {/* Titles */}
 
-                  <span className="ml-2">{item.label}</span>
+                  {/* Titles */}
+                  <span className="ml-2 hidden sm:inline">{item.label}</span>
                 </div>
 
                 {item.active && (
-                  <div className="w-2 h-2 bg-[#6154F0] rounded-full"></div>
+                  <div className="w-2 h-2 bg-[#6154F0] rounded-full hidden sm:block"></div>
                 )}
               </button>
             </div>
